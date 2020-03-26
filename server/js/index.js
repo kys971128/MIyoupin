a5362f729472f6638a345128fd8c94c724deafaa
kys <934535885@qq.com>
2020-03-27 00:16:06 +0800

    小米有品项目完成到轮播图，2020.3.26

---
+++ server/js/index.js
@@ -0,0 +1,1 @@
+"use strict";$("#search").on("click",function(e){return $(this).css("borderColor","#8C5D3D"),!1}),$(document).on("click",function(){$("#search").css("borderColor","")}),$("#slide").banner({img:$("#slide").children("#imgbox").children("a"),left:$("#slide").find("#left_btn"),right:$("#slide").find("#right_btn"),list:!0,autoPlay:!0,delayTime:3e3,moveTime:300});
